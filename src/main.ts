1cad9db80c80aca84fd0c225ad22c6626c9fba57
sheria <sheriajason343@gmail.com>
2025-06-11 19:57:40 +0100

    first sec

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,10 @@ async function bootstrap() {
     origin: [
       'http://127.0.0.1:5500',
       'http://localhost:5500',
-      'https://wise-technology.onrender.com'
+      'https://wise-technology.onrender.com',
+      'https://wise-hosting.onrender.com/',
+      'https://wise-technology.onrender.com',
+      '*' 
     ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
